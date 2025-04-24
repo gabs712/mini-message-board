f7207148f2b6a81cbc4f30a61ead2bb39618597a
gabs <gabs712.g@gmail.com>
2025-04-24 18:36:42 -0300

    Change date format for populate module

--- db/populate.js
+++ db/populate.js
@@ -7,13 +7,13 @@ CREATE TABLE IF NOT EXISTS messages (
   id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
   username VARCHAR ( 255 ),
   text TEXT,
-  added DATE
+  time TIMESTAMP
 );
 
-INSERT INTO messages (username, text, added) 
+INSERT INTO messages (username, text, time) 
 VALUES
-  ('Amando', 'Hi there!', NOW()),
-  ('Damon', 'Hello World!', NOW());
+  ('Amando', 'Hi there!', DATE_TRUNC('second', NOW())),
+  ('Damon', 'Hello World!', DATE_TRUNC('second', NOW()));
 `
 
 async function main() {
